738f1ba9e445cbccfba61b0e5f931079e483110c
Willian Cardoso <willian.b.cardoso@gmail.com>
2025-06-05 12:27:35 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Cria ambiente virtual
-python3 -m venv venv
-source venv/bin/activate
+# Cria o ambiente virtual
+python3 -m venv .venv
+source .venv/bin/activate
 
-# Instala dependências com workaround pro erro da PEP 668
-pip install --break-system-packages --upgrade pip
+# Força o pip a instalar mesmo com restrição do sistema
+pip install --upgrade pip
 pip install --break-system-packages -r requirements.txt
 
-# Roda o app
-venv/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
+# Inicia o FastAPI com uvicorn do ambiente virtual
+.venv/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
