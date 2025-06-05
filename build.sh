9a79c907dff2bc297369bd08f53467031ba8ab1c
Willian Cardoso <willian.b.cardoso@gmail.com>
2025-06-05 12:11:39 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# Cria e ativa ambiente virtual
+python3 -m venv venv
+source venv/bin/activate
+
+# Instala dependências no ambiente virtual
+pip install --upgrade pip
 pip install -r requirements.txt
-python3 -m uvicorn main:app --host 0.0.0.0 --port $PORT
+
+# Roda a aplicação usando uvicorn do ambiente virtual
+venv/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
